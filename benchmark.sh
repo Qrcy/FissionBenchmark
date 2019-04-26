ab94d37bf04497453f50f75a6220f3a29412146f
Kurucz Gábor <48492301+Qrcy@users.noreply.github.com>
2019-04-26 17:13:12 +0200

    update

--- benchmark.sh
+++ benchmark.sh
@@ -4,15 +4,23 @@
 # @created: 2019-03-05
 # @version: 1.0
 
-functions=(hellojava hellogo hellopython primjava)
+functionsGET=(hellojava hellogo hellopython)
+functionsPOST=(primjava)
 connections=(50)
 times=(1m)
+dt=`date '+%d/%m/%Y %H:%M:%S'`
 fissionrouter="node1:32695"
 
-echo -e "Benchmarking functions\n"
-for function in "${functions[@]}"
+echo -e "Benchmarking functions GET\n"
+for functionGET in "${functionsGET[@]}"
 do
-    echo -e "Benchmarking $function\n"
-    echo -e "hey-summary\n"
-    hey -c $connections -z $times -m POST -d '{"id":"44444"}' http://$fissionrouter/$function > ./logs/$function.$connection.$times.$datetime.hey.txt
+    echo -e "Benchmarking $functionGET\n"
+    hey -c $connections -z $times -m GET http://$fissionrouter/$functionGET > ./logs/$functionGET.hey.txt
+done
+
+echo -e "Benchmarking functions POST\n"
+for functionPOST in "${functionsPOST[@]}"
+do
+    echo -e "Benchmarking $functionPOST\n"
+    hey -c $connections -z $times -m POST -d '{"id":"44444"}' http://$fissionrouter/$functionPOST > ./logs/$functionPOST.hey.txt
 done
