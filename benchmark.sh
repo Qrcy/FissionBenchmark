4003af3553a08cd5a3a7e88d9bffe8f07852c930
Kurucz Gábor <48492301+Qrcy@users.noreply.github.com>
2019-05-09 01:52:06 +0200

    update

--- benchmark.sh
+++ benchmark.sh
@@ -12,23 +12,23 @@ times=(1m)
 now=$(date +"%d/%m/%Y %H:%M:%S")
 fissionrouter="192.168.0.10:30260"
 
-echo -e "Benchmarking functions GET\n"
+echo -e "Benchmarking functions GET Hello"
 for functionGET in "${functionsGET[@]}"
 do
-    echo -e "Benchmarking $functionGET\n"
+    echo -e "Benchmarking $functionGET"
     hey -c $connections -z $times -m GET http://$fissionrouter/$functionGET > ./logs/$functionGET.$now.hey.txt
 done
 
-echo -e "Benchmarking functions POST\n"
+echo -e "Benchmarking functions POST Prim\n"
 for functionPOST in "${functionsPOST[@]}"
 do
-    echo -e "Benchmarking $functionPOST\n"
+    echo -e "Benchmarking $functionPOST"
     hey -c $connections -z $times -m POST -d '{"id":"45"}' http://$fissionrouter/$functionPOST > ./logs/$functionPOST.$now.hey.txt
 done
 
-echo -e "Benchmarking functions GET\n"
-for functionGET in "${functionsGET[@]}"
+echo -e "Benchmarking functions GET Matrix\n"
+for functionGET2 in "${functionsGET2[@]}"
 do
-    echo -e "Benchmarking $functionGET\n"
-    hey -c $connections -z $times -m GET http://$fissionrouter/$functionGET > ./logs/$functionGET.$now.hey.txt
+    echo -e "Benchmarking $functionGET2"
+    hey -c $connections -z $times -m GET http://$fissionrouter/$functionGET2 > ./logs/$functionGET2.$now.hey.txt
 done
