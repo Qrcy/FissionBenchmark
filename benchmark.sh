fb03e8bec1db724f0dac30d0c400eddcbfac9cb3
Kurucz Gábor <48492301+Qrcy@users.noreply.github.com>
2019-05-09 18:41:45 +0200

    update

--- benchmark.sh
+++ benchmark.sh
@@ -8,27 +8,27 @@ functionsGET=(khellojava khellogo khellopython)
 functionsPOST=(kprimjava kprimgo kprimpython)
 functionsGET2=(kmatrixjava kmatrixgo kmatrixpython)
 connections=(100)
-times=(20s)
-now=$(date +"%d/%m/%Y %H:%M:%S")
+times=(5m)
+now=$(date +"%m_%d_%Y_%H:%M:%S")
 fissionrouter="192.168.0.10:30260"
 
 echo -e "Benchmarking functions GET Hello"
 for functionGET in "${functionsGET[@]}"
 do
     echo -e "Benchmarking $functionGET"
-    hey -c $connections -z $times -m GET http://$fissionrouter/$functionGET > ./logs/$functionGET.$now.hey.txt
+    hey -c $connections -z $times -m GET http://$fissionrouter/$functionGET > ./logs/$functionGET.C.$connections.T.$times.$now.hey.txt
 done
 
 echo -e "Benchmarking functions POST Prim\n"
 for functionPOST in "${functionsPOST[@]}"
 do
     echo -e "Benchmarking $functionPOST"
-    hey -c $connections -z $times -m POST -H "Content-Type: application/json" -d '{"id":"45"}' http://$fissionrouter/$functionPOST > ./logs/$functionPOST.$now.hey.txt
+    hey -c $connections -z $times -m POST -H "Content-Type: application/json" -d '{"id":"45"}' http://$fissionrouter/$functionPOST > ./logs/$functionPOST.C.$connections.T.$times.$now.hey.txt
 done
 
 echo -e "Benchmarking functions GET Matrix\n"
 for functionGET2 in "${functionsGET2[@]}"
 do
     echo -e "Benchmarking $functionGET2"
-    hey -c $connections -z $times -m GET http://$fissionrouter/$functionGET2 > ./logs/$functionGET2.$now.hey.txt
+    hey -c $connections -z $times -m GET http://$fissionrouter/$functionGET2 > ./logs/$functionGET2.C.$connections.T.$times.$now.hey.txt
 done
